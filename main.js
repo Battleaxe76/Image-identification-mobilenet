1fdfd9a3ec45d549343af79262162f039d6499f0
Battleaxe76 <70934717+Battleaxe76@users.noreply.github.com>
2020-10-13 16:43:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+Webcam.set({
+    width:310,
+    height:300,
+    image_format:"png",
+    png_quality:90,
+    constraints:{
+        Facingmode:"environment"
+    }
+});
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+function take_snapshot(){
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+console.log('ml5 version',ml5.version);
+
+function modelLoaded(){
+    console.log("modelloaded")
+}
