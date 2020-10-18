3d744379f323409009fb1452957d0725e8117ccc
Battleaxe76 <70934717+Battleaxe76@users.noreply.github.com>
2020-10-18 15:24:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,22 @@ function take_snapshot(){
     });
 }
 console.log('ml5 version',ml5.version);
-
+classifier=ml5.imageClassifier('mobilenet',modelLoaded);
 function modelLoaded(){
     console.log("modelloaded")
+}
+
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+if (error){
+    console.error(error)
+}
+else{
+    console.log(results)
+    document.getElementById("object_name").innerHTML=results[0].label;
+}
+
 }
